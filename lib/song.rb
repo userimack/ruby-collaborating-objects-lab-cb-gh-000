9b3a86ba3e9677d357daaf339549874c8c018d9f
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-09 18:11:11 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name, :artist 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song.save
+    new_song
+  end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save 
+    @@all << self 
+    self
+  end 
+end
+    
